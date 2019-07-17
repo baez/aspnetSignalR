5013308b44c28555f9a05bcc1a1ee2844971f10c
Behzad Karim <bekarim@microsoft.com>
2019-07-17 16:15:35 -0700

    Created the initial project.

---
+++ AspnetSignalR/Models/ArticleActivity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspnetSignalR.Models
+{
+    public class ArticleActivity
+    {
+        public string Id { get; set; }
+        public int ViewCount { get; set; }
+
+    }
+}
---
+++ AspnetSignalR/SignalRHubs/ArticleHub.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using AspnetSignalR.Models;
+using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+
+namespace AspnetSignalR.SignalRHubs
+{
+    public class ArticleHub : Hub
+    {
+        private static readonly ConcurrentDictionary<string, ArticleActivity> articleActivities = new ConcurrentDictionary<string, ArticleActivity>();
+
+        public async Task GetArticlePageViews(string articleId)
+        {
+            ArticleActivity articleActivity;
+            var result = articleActivities.TryGetValue(articleId, out articleActivity);
+            if (result && articleActivity != null)
+            {
+                articleActivity.ViewCount++;
+            }
+            else
+            {
+                articleActivity = new ArticleActivity() { Id = articleId, ViewCount = 1 };
+            }
+
+            articleActivities.AddOrUpdate(articleId, articleActivity, (key, activity) => activity);
+
+            await Clients.Others.Update();
+        }
+    }
+}
